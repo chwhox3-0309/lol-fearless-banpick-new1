2a2d33a7fdd58b32d71e70561883b45ce85e00a4
chwhox3-0309 <chwhox3@gmail.com>
2025-10-20 20:12:57 +0900

    chore(build): Temporarily remove custom headers from next.config.js for debugging

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,6 @@ const nextConfig = {
     domains: ['ddragon.leagueoflegends.com'],
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `frame-ancestors 'self' *.googlesyndication.com *.google.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com *.googlesyndication.com *.googleadservices.com *.adtrafficquality.google;`,
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
